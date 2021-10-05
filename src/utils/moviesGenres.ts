fece3a714d8f48a051831ab0566dade14ffd9e0d
Pablo Henrique <pablo_hen@hotmail.com>
2021-10-05 12:57:30 -0300

    add genresAlphabet

--- src/utils/moviesGenres.ts
+++ src/utils/moviesGenres.ts
@@ -15,66 +15,77 @@ const genres = [
     id: 35,
     name: 'Comédia',
   },
-  // {
-  //   id: 80,
-  //   name: 'Crime',
-  // },
-  // {
-  //   id: 99,
-  //   name: 'Documentário',
-  // },
-  // {
-  //   id: 18,
-  //   name: 'Drama',
-  // },
-  // {
-  //   id: 10751,
-  //   name: 'Família',
-  // },
-  // {
-  //   id: 14,
-  //   name: 'Fantasia',
-  // },
-  // {
-  //   id: 36,
-  //   name: 'História',
-  // },
-  // {
-  //   id: 27,
-  //   name: 'Terror',
-  // },
-  // {
-  //   id: 10402,
-  //   name: 'Música',
-  // },
-  // {
-  //   id: 9648,
-  //   name: 'Mistério',
-  // },
-  // {
-  //   id: 10749,
-  //   name: 'Romance',
-  // },
-  // {
-  //   id: 878,
-  //   name: 'Ficção científica',
-  // },
-  // {
-  //   id: 10770,
-  //   name: 'Cinema TV',
-  // },
-  // {
-  //   id: 53,
-  //   name: 'Thriller',
-  // },
-  // {
-  //   id: 10752,
-  //   name: 'Guerra',
-  // },
-  // {
-  //   id: 37,
-  //   name: 'Faroeste',
-  // },
+  {
+    id: 80,
+    name: 'Crime',
+  },
+  {
+    id: 99,
+    name: 'Documentário',
+  },
+  {
+    id: 18,
+    name: 'Drama',
+  },
+  {
+    id: 10751,
+    name: 'Família',
+  },
+  {
+    id: 14,
+    name: 'Fantasia',
+  },
+  {
+    id: 36,
+    name: 'História',
+  },
+  {
+    id: 27,
+    name: 'Terror',
+  },
+  {
+    id: 10402,
+    name: 'Música',
+  },
+  {
+    id: 9648,
+    name: 'Mistério',
+  },
+  {
+    id: 10749,
+    name: 'Romance',
+  },
+  {
+    id: 878,
+    name: 'Ficção científica',
+  },
+  {
+    id: 10770,
+    name: 'Cinema TV',
+  },
+  {
+    id: 53,
+    name: 'Thriller',
+  },
+  {
+    id: 10752,
+    name: 'Guerra',
+  },
+  {
+    id: 37,
+    name: 'Faroeste',
+  },
 ];
+
+const genresAlphabet: string[] = genres
+  .map((genre) => genre.name[0])
+  .reduce(
+    (genres, genre) =>
+      genres.includes(genre) ? [...genres] : [...genres, genre],
+    []
+  )
+  .sort();
+
+export { genresAlphabet };
 
 export default genres;
